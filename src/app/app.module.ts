b9a296ec8f4b81e7b62d84b92b032a82d993c868
Ghufran Hasan <ghufran.aims@gmail.com>
2022-11-23 12:43:45 +0500

    Main app component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { MyContainerComponent } from './myContainer/myContainer.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyContainerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
