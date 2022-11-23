18ce2e091c9102c06a3de244a45e7a3bc72f1911
Ghufran Hasan <ghufran.aims@gmail.com>
2022-11-23 15:15:48 +0500

    TopNav declaration in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './myContainer/topnav/topnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyContainerComponent
+    MyContainerComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
