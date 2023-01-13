929ec2b15fd1d7b527c9f3d047605aa3db322d00
Ghufran Hasan <ghufran.aims@gmail.com>
2023-01-13 11:48:54 +0500

    component lifecycle hooks.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,4 +12,15 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  value!:string;
+  comp1Exist: boolean = true;
+
+  submitValue(val: { value: string; }){
+    this.value = val.value
+  }
+
+  destroy(){
+    this.comp1Exist = false;
+  }
+
 }
--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 import { DesignutilityService } from '../appServices/designutility.service';
 
 @Component({
@@ -6,22 +6,66 @@ import { DesignutilityService } from '../appServices/designutility.service';
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.css']
 })
-export class Comp1Component implements OnInit {
+export class Comp1Component implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor(private _designServices: DesignutilityService) {
-    this._designServices.userName.subscribe(uname => {
-      this.userName = uname
-    })
+  // constructor(private _designServices: DesignutilityService) {
+  //   this._designServices.userName.subscribe(uname => {
+  //     this.userName = uname
+  //   })
+  //  }
+
+  @Input() myValue = "UxTrendz"
+  counter!: any;
+  num: number = 1;
+
+   constructor() {
+    console.log("constructor called")
    }
 
   ngOnInit(): void {
+    this.counter = setInterval(() => {
+      this.num = this.num + 1
+      console.log(this.num)
+    }, 1000)
+    console.log("ngOnInit called")
   }
 
-  userName!: string
-
-  updateUserName(uname: { value: string; }) {
-    // this.userName = uname.value
-    this._designServices.userName.next(uname.value)
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log("ngOnChanges called")
+    console.log("ngOnChanges called" + " " + changes['myValue'].currentValue)
+    // console.log(changes)
+    // console.log(changes['myValue'].currentValue)
   }
 
+  ngDoCheck(): void {
+    console.log("ngDoCheck called")
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit called")
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked called")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit called")    
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked called")
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy called")
+    clearInterval(this.counter)
+  }
+
+  // userName!: string
+
+  // updateUserName(uname: { value: string; }) {
+  //   // this.userName = uname.value
+  //   this._designServices.userName.next(uname.value)
+  // }
 }
